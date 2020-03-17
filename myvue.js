fa8052fc48e735b261cb88bba6e1fba8f84cccca
WongYuYe <wyuye_beself@qq.com>
2020-03-18 00:41:12 +0800

    3rd

--- myvue.js
+++ myvue.js
@@ -1,202 +1,186 @@
-// compile方法类
-const compileUtil = {
-  text(node, expr, vm) {
-    let newVal;
-    if (expr.includes("{{")) {
-      newVal = expr.replace(/\{\{(.+?)\}\}/g, (...match) => {
-        new Watcher(match[1].trim(), vm, () => {
-          this.updater.textUpdater(node, this.getNewVal(expr, vm));
-        });
-        return this.getVal(match[1].trim(), vm);
-      });
-    } else {
-      new Watcher(expr, vm, newVal => {
-        this.updater.textUpdater(node, newVal);
-      });
-      newVal = this.getVal(expr, vm);
-    }
-    this.updater.textUpdater(node, newVal);
-  },
-  html(node, expr, vm) {
-    new Watcher(expr, vm, newVal => {
-      this.updater.htmlUpdater(node, newVal);
-    });
-    let newVal;
-    newVal = this.getVal(expr, vm);
-    this.updater.htmlUpdater(node, newVal);
-  },
-  model(node, expr, vm) {
-    new Watcher(expr, vm, newVal => {
-      this.updater.modelUpdater(node, newVal);
-    });
-    let newVal;
-    newVal = this.getVal(expr, vm);
-    this.updater.modelUpdater(node, newVal);
-    node.addEventListener(
-      "input",
-      e => {
-        // this.updater.modelUpdater(node, e.target.value);
-        this.setVal(expr, vm, e.target.value);
-      },
-      false
-    );
-  },
-  bind(node, expr, vm, attrName) {
-    new Watcher(expr, vm, newVal => {
-      this.updater.bindUpdater(node, attrName, newVal);
-    });
-    let newVal;
-    newVal = this.getVal(expr, vm);
-    this.updater.bindUpdater(node, attrName, newVal);
-  },
-  on(node, expr, vm, eventName) {
-    const fn = vm.$options.methods && vm.$options.methods[expr];
-    node.addEventListener(eventName, fn.bind(vm), false);
-  },
-  getVal(expr, vm) {
-    return expr.split(".").reduce((data, currentVal) => {
-      return data[currentVal];
-    }, vm.$data);
-  },
-  getNewVal(expr, vm) {
-    return expr.replace(/\{\{(.+?)\}\}/g, (...match) => {
-      return this.getVal(match[1].trim(), vm);
-    });
-  },
-  setVal(expr, vm, newVal) {
-    expr.split(".").reduce((data, currentVal) => {
-      data[currentVal] = newVal;
-    }, vm.$data);
-  },
-  updater: {
-    textUpdater(node, textVal) {
-      node.textContent = textVal;
-    },
-    htmlUpdater(node, htmlVal) {
-      node.innerHTML = htmlVal;
-    },
-    modelUpdater(node, modelVal) {
-      node.value = modelVal;
-    },
-    bindUpdater(node, attrName, attrVal) {
-      node.setAttribute(attrName, attrVal);
-    }
-  }
-};
-
-// Compile类
 class Compile {
   constructor(el, vm) {
     this.$vm = vm;
 
-    this.$el = this.isElementNode(el) ? el : document.querySelector(el);
+    // 获取节点
+    this.$el = this.isElementNode(el) ? el : document.querySelector(el)
 
-    // 转化为文档碎片， 防止重绘和回流
-    const frag = this.node2Fragment(this.$el);
+    // 节点转化为文档片段，防止重绘和回流
+    const frag = this.node2Fragment(this.$el)
 
-    // 返回编译过的文档碎片
-    const compileFrag = this.compile(frag);
+    // 编译文档片段
+    const compileFrag = this.compile(frag)
 
-    // 挂载到节点上
-    this.$el.appendChild(compileFrag);
+    // 挂载到根节点上
+    this.$el.appendChild(compileFrag)
   }
-  isElementNode(node) {
-    return node.nodeType === 1;
+  isElementNode(v) {
+    return v.nodeType === 1
+  }
+  isTextNode(v) {
+    return v.nodeType === 3
   }
   node2Fragment(node) {
     const frag = document.createDocumentFragment();
     let firstChild;
-    while ((firstChild = node.firstChild)) {
-      frag.appendChild(firstChild);
+    while (firstChild = node.firstChild) {
+      frag.appendChild(firstChild)
     }
-    return frag;
+    return frag
   }
-  // 是否为vue指令
-  isVueDirective(attrName) {
-    return attrName.startsWith("v-");
-  }
-  // 是否为事件名
-  isEventName(attrName) {
-    return attrName.startsWith("@");
-  }
-  // 编译模板
   compile(frag) {
-    [...frag.childNodes].forEach(child => {
-      if (child.nodeType === 1) {
+    [...frag.childNodes].forEach(childNode => {
+      if (this.isElementNode(childNode)) {
         // 元素节点
-        this.compileElement(child);
-      } else if (child.nodeType === 3) {
+        this.compileElement(childNode)
+      } else if (this.isTextNode(childNode)) {
         // 文本节点
-        this.compileText(child);
+        this.compileText(childNode)
       }
-      // 递归编译
-      if (child.childNodes && child.childNodes.length) {
-        this.compile(child);
+      if (childNode.childNodes && childNode.childNodes.length) {
+        this.compile(childNode)
       }
-    });
-    return frag;
+    })
+    return frag
   }
-  // 编译元素节点
+  isVueDirective(v) {
+    return v.startsWith('v-')
+  }
+  isVueEventDirective(v) {
+    return v.startsWith('@')
+  }
   compileElement(node) {
-    const attributes = node.attributes;
+    const { attributes } = node;
+    // console.log(attributes);
     [...attributes].forEach(attr => {
       const { name, value } = attr;
-      // 判断vue指令如v-text v-model v-html v-on:click v-bind:src
+      // 判断是否为v指令，如v-text, v-html, v-model, v-bind:src, v-on:click
       if (this.isVueDirective(name)) {
-        // 拆分属性
-        const [, directive] = name.split("-");
-        const [dirName, eventName] = directive.split(":");
-
-        compileUtil[dirName](node, value, this.$vm, eventName);
-      } else if (this.isEventName(name)) {
-        const [, eventName] = name.split("@");
-
-        compileUtil["on"](node, value, this.$vm, eventName);
+        const [, directive] = name.split('-')
+        // 再分割':'
+        const [dirName, eventName] = directive.split(':');
+        compileUtils[dirName](node, value, this.$vm, eventName)
+      } else if (this.isVueEventDirective(name)) {
+        // 判断是否为@事件指令，如@click
+        const [, eventName] = name.split('@')
+        compileUtils['on'](node, value, this.$vm, eventName)
       }
-      // 移除指令属性
-      node.removeAttribute(name);
-    });
+    })
   }
   compileText(node) {
     const reg = /\{\{(.+?)\}\}/g;
     if (reg.test(node.textContent)) {
-      compileUtil["text"](node, node.textContent, this.$vm);
+      compileUtils["text"](node, node.textContent, this.$vm);
     }
   }
 }
-
-// Vue类
+const compileUtils = {
+  text(node, expr, vm) {
+    let v;
+    if ((/\{\{(.+?)\}\}/g).test(expr)) {
+      v = expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+        new Watcher(args[1].trim(), vm, () => {
+          console.log(this.getNewVal(expr, vm))
+          this.updater.textUpdater(node, this.getNewVal(expr, vm))
+        })
+        return this.getVal(args[1].trim(), vm)
+      })
+    } else {
+      new Watcher(expr, vm, v => {
+        this.updater.textUpdater(node, v)
+      })
+      v = this.getVal(expr, vm)
+    }
+    this.updater.textUpdater(node, v)
+  },
+  html(node, expr, vm) {
+    new Watcher(expr, vm, v => {
+      this.updater.htmlUpdater(node, v)
+    })
+    this.updater.htmlUpdater(node, this.getVal(expr, vm))
+  },
+  model(node, expr, vm) {
+    new Watcher(expr, vm, v => {
+      this.updater.modelUpdater(node, v)
+    })
+    this.updater.modelUpdater(node, this.getVal(expr, vm))
+    node.addEventListener('input', (e) => {
+      this.setVal(expr, vm, e.target.value)
+    })
+  },
+  bind(node, expr, vm, attr) {
+    new Watcher(expr, vm, v => {
+      this.updater.bindUpdater(node, v, attr)
+    })
+    this.updater.bindUpdater(node, this.getVal(expr, vm), attr)
+  },
+  on(node, expr, vm, eventName) {
+    const fn = vm.$methods && vm.$methods[expr]
+    node.addEventListener(eventName, fn.bind(vm), false)
+  },
+  getVal(expr, vm) {
+    return expr.split('.').reduce((data, currentVal) => {
+      return data[currentVal]
+    }, vm.$data)
+  },
+  getNewVal(expr, vm) {
+    return expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+      return this.getVal(args[1].trim(), vm)
+    })
+  },
+  setVal(expr, vm, v) {
+    expr.split('.').reduce((data, currentVal) => {
+      data[currentVal] = v
+    }, vm.$data)
+  },
+  updater: {
+    textUpdater(node, v) {
+      node.textContent = v
+    },
+    htmlUpdater(node, v) {
+      node.innerHTML = v
+    },
+    modelUpdater(node, v) {
+      node.value = v
+    },
+    bindUpdater(node, v, attr) {
+      node.setAttribute(attr, v)
+    }
+  }
+}
 class Vue {
   constructor(options) {
-    this.$options = options;
     this.$el = options.el;
     this.$data = options.data;
+    this.$methods = options.methods;
+    this.$options = options;
 
-    // 当有el时再进行下一步
     if (this.$el) {
       // 数据劫持
-      new Observer(this.$data);
+      new Observer(this.$data)
       // 编译模板
-      new Compile(this.$el, this);
+      new Compile(this.$el, this)
 
-      // 数据代理 this.$data => this
-      this.proxyData(this.$data);
+      // 代理数据
+      this.proxyData(this.$data)
     }
   }
   proxyData(data) {
-    Reflect.ownKeys(data).forEach(key => {
-      Object.defineProperty(this, key, {
-        enumerable: true,
-        configurable: false,
-        get() {
-          return data[key];
-        },
-        set(newVal) {
-          if (newVal !== data[key]) {
-            data[key] = newVal;
+    if (typeof data === 'object' && data !== null) {
+      Reflect.ownKeys(data).forEach(key => {
+        Object.defineProperty(this, key, {
+          enumerable: true,
+          configurable: false,
+          get() {
+            return data[key]
+          },
+          set(newVal) {
+            if (newVal !== data[key]) {
+              data[key] = newVal
+            }
           }
-        }
-      });
-    });
+        })
+      })
+    }
   }
 }
--- observer.js
+++ observer.js
@@ -1,71 +1,65 @@
-// 数据劫持
+class Watcher {
+  constructor(expr, vm, cb) {
+    this.$expr = expr;
+    this.$vm = vm;
+    this.$cb = cb;
+
+    // 获取第一次编译时的值
+    this.oldVal = this.getOldVal()
+  }
+  getOldVal() {
+    Dep.target = this;
+    const oldVal = compileUtils.getVal(this.$expr, this.$vm)
+    Dep.target = null;
+    return oldVal
+  }
+  update() {
+    const v = compileUtils.getVal(this.$expr, this.$vm)
+    if (v !== this.oldVal) {
+      this.$cb(v)
+    }
+  }
+}
+class Dep {
+  constructor() {
+    // 用数组来存放watcher
+    this.subs = []
+  }
+  addSubs(w) {
+    this.subs.push(w)
+  }
+  notify() {
+    this.subs.forEach(w => w.update())
+  }
+}
 class Observer {
   constructor(data) {
-    this.observe(data);
+    this.observe(data)
   }
   observe(data) {
-    if (typeof data === "object" && data !== null) {
+    if (typeof data === 'object' && data !== null) {
       Reflect.ownKeys(data).forEach(key => {
-        this.defineReactive(data, key, data[key]);
-      });
+        this.defineReactive(data, key, data[key])
+      })
     }
   }
   defineReactive(data, key, value) {
-    this.observe(value);
-    const dep = new Dep();
+    this.observe(value)
+    const dep = new Dep()
     Object.defineProperty(data, key, {
-      enumerable: true,
-      configurable: false,
+      enumerable: true, // 是否可枚举
+      configurable: false, // 是否可被删除
       get() {
-        Dep.target && dep.addSub(Dep.target);
-        return value;
+        Dep.target && dep.addSubs(Dep.target)
+        return value
       },
       set: (newVal) => {
         this.observe(newVal)
         if (newVal !== value) {
-          value = newVal;
+          value = newVal
         }
-        dep.notify();
+        dep.notify()
       }
-    });
-  }
-}
-
-// 观察者
-class Watcher {
-  constructor(expr, vm, cb) {
-    this.expr = expr;
-    this.vm = vm;
-    this.cb = cb;
-    this.oldVal = this.getOldVal();
-  }
-  // 获取初始化的旧值
-  getOldVal() {
-    Dep.target = this;
-    const oldVal = compileUtil.getVal(this.expr, this.vm);
-    Dep.target = null
-    return oldVal
-  }
-  // 更新，执行含新值newVal的callback
-  update() {
-    const newVal = compileUtil.getVal(this.expr, this.vm);
-    if (newVal !== this.oldVal) {
-      this.cb(newVal);
-    }
-  }
-}
-
-// 依赖收集器
-class Dep {
-  constructor() {
-    this.subs = []; // 初始化空数组存放watcher
-  }
-  addSub(watcher) {
-    this.subs.push(watcher);
-  }
-  // 通知watcher更新
-  notify() {
-    console.log('subs: ' + this.subs)
-    this.subs.forEach(w => w.update());
+    })
   }
 }
